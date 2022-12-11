7daf9ae24ffd32d0f0b37944734dca0f42aa28f2
Dekunov Aleksandr <wresda@gmail.com>
2022-12-11 22:39:18 +0300

    'l'

--- server.py
+++ server.py
@@ -1,22 +1,22 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+# from flask_sqlalchemy import SQLAlchemy
+# from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://b2bsite.db'
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://b2bsite.db'
+# db = SQLAlchemy(app)
 
 
-class Article(db.Model):
-    id = db.Colum(db.Integer, primary_key=True)
-    title = db.Colum(db.String(100), nullable=False)
-    intro = db.Colum(db.String(300), nullable=False)
-    text = db.Colum(db.Text, nullable=False)
-    date = db.Colum(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        '''выдавать объект и его ID'''
-        return '<Article %r>' % self.id
+# class Article(db.Model):
+#     id = db.Colum(db.Integer, primary_key=True)
+#     title = db.Colum(db.String(100), nullable=False)
+#     intro = db.Colum(db.String(300), nullable=False)
+#     text = db.Colum(db.Text, nullable=False)
+#     date = db.Colum(db.DateTime, default=datetime.utcnow)
+#
+#     def __repr__(self):
+#         '''выдавать объект и его ID'''
+#         return '<Article %r>' % self.id
 
 
 @app.route('/')
